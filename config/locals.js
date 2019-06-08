e854cd907039070e1975a9b6a7fa2fc200b19e24
Tobiasz <42369349+TobiaszTop@users.noreply.github.com>
2019-06-08 17:26:23 +0200

    add auto-update

--- config/locals.js
+++ config/locals.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   app.locals.site = {
-    title: "Ex-situ Conservation",
+    title: "Breeding and Reintroduction | Ex-situ Conservation",
     description: "Captive breeding and reintroduction as a conservation method"
   };
 };
