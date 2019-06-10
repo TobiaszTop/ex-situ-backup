bdb83dc7463395c988ede71819bc2caf2412ada8
Tobiasz <42369349+TobiaszTop@users.noreply.github.com>
2019-06-10 02:36:36 +0200

    add page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,26 @@
-var numberOfLinks = document.querySelectorAll('nav.top li').length;
+if(document.querySelector('main#examples')){
+  var numberOfButtons = document.querySelectorAll('#examples button').length;
 
-for(var i = 2; i<numberOfLinks+1; i++){
-  var link = document.querySelector(`nav.top li:nth-child(${i}) a`);
+  for(var i=1; i<numberOfButtons+1; i++){
+    var button = document.querySelector(`#examples button:nth-of-type(${i})`);
+    button.setAttribute('name', i);
+    button.addEventListener('click', function(){
+      for(var i=1; i<numberOfButtons+1; i++){
+        var section = document.querySelector(`#examples div.ex:nth-of-type(${i})`);
+        var button = document.querySelector(`#examples button:nth-of-type(${i})`);
+        section.classList.remove('show');
+        button.classList.remove('show');
+      }
+      var n = this.getAttribute('name');
+      document.querySelector(`#examples button:nth-of-type(${n})`).classList.add('show');
+      document.querySelector(`#examples div.ex:nth-of-type(${n})`).classList.add('show');
+    });
+  }
+}
+
+var numberOfLinks = document.querySelectorAll('nav.top li').length;
+for(var i=2; i<numberOfLinks+1; i++){
+  var link = document.querySelector(`nav.top li:nth-of-type(${i}) a`);
 
   if(window.location.href.indexOf(link.getAttribute('href'))>1){
     link.classList.add('current');
